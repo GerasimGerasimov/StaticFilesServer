8e0d03a310bfc76d0970c42a7750b36da0a27f32
Gerasim Gerasimov <dialix@yandex.ru>
2020-09-25 16:18:00 +0700

    add source

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express()
+
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
+app.use(express.static(__dirname + '/build'));
+
+console.log('server started at 4000')
+app.listen(4000)
