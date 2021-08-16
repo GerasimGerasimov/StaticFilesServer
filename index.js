a6c2bd007525edb89da865f6cc41ac8293f42be1
GerasimGerasimov <dialix@yandex.ru>
2021-08-16 11:00:41 +0100

    решена проблема CORS

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express")
+const cors = require ("cors");
+
 const app = express()
 
 let NodePath = process.argv[0];
@@ -6,20 +8,13 @@ let AppPath = process.argv[1];
 let ServerPort = process.argv[2];
 let FrontEndPath = process.argv[3];
 
-
 console.log(`Sterting HTTP Server...`)
 console.log(`  AppPath: ${AppPath}`)
 console.log(`  ServerPort: ${ServerPort}`)
 console.log(`  FrontEndPath: ${FrontEndPath}`)
 
-app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
-
-app.use(express.static(FrontEndPath));//__dirname + '/build'));
+app.use(cors({ origin: true }));
+app.use(express.static(FrontEndPath));
 
 console.log(`HTTP Server started at: ${ServerPort}`)
 app.listen(ServerPort)
