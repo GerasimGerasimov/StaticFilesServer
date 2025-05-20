73234ce5a80c36dff085a2eed60229c5701cd200
cest0de <andreynikonorov5@gmail.com>
2025-05-20 17:29:21 +0700

    Перевод фронтенда на https

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express")
-const cors = require ("cors");
+const cors = require("cors");
+const https = require("node:https");
+const fs = require("node:fs");
+const path = require("node:path");
 
 const app = express()
 
@@ -8,7 +11,7 @@ let AppPath = process.argv[1];
 let ServerPort = process.argv[2];
 let FrontEndPath = process.argv[3];
 
-console.log(`Sterting HTTP Server...`)
+console.log(`Starting HTTP Server...`)
 console.log(`  AppPath: ${AppPath}`)
 console.log(`  ServerPort: ${ServerPort}`)
 console.log(`  FrontEndPath: ${FrontEndPath}`)
@@ -17,4 +20,10 @@ app.use(cors({ origin: true }));
 app.use(express.static(FrontEndPath));
 
 console.log(`HTTP Server started at: ${ServerPort}`)
-app.listen(ServerPort)
+
+const cert = fs.readFileSync(path.resolve(__dirname, "./cert/_.intmash.ru.crt"));
+const key = fs.readFileSync(path.resolve(__dirname, "./cert/private_key.key"));
+https.createServer({
+    cert,
+    key
+}, app).listen(ServerPort)
